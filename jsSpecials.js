512d509a0766bdad970ac8538881d9f558559e4c
Johnson Saka <sakajohnson1@gmail.com>
2024-05-24 15:41:39 +0000

    functions in JS

---
+++ jsSpecials.js
@@ -0,0 +1,39 @@
+/*let x = 23;
+console.log(x);*/
+
+//let myNumber = 100; // camelcase way of naming a variable
+
+//Interaction
+/*let myQuestion = prompt("How old are you" , "");
+
+if (myQuestion > 20){
+    alert("You are mature");
+}else {
+    alert("You're young");
+}*/
+
+// result of a prompt is a string
+
+
+//functions 
+/*function mySum ( x , y){
+    return x + y ;
+
+};
+
+
+let addition = mySum(2,3);
+console.log(addition);*/  //function declaration
+
+//function expression 
+
+/*let sum = function (a , b){
+    let result = a+b;
+    return result ;
+};
+
+console.log(sum(2,3)); */
+
+let sum = ( a , b) => {
+    return a + b ;
+};
