61929bca3591830b2b74cd281222be7fa2d6377f
Johnson <sakajohnson1@gmail.com>
2024-12-26 11:01:52 +0000

    javascript callbacks, maps, for ech

--- Objref.js
+++ Objref.js
@@ -50,3 +50,90 @@ user.sayHi();*/
 user.sayHi = sayHi;
 
 user.sayHi();*/
+
+// template literal
+
+/*let string1 = 'Hello';
+let string2 = 'Walker';
+
+let greetings = `${string1} ${string2}` ;
+console.log(greetings);*/
+
+
+/*function myGreeting(name,salutation= "Hello"){
+    console.log(`${salutation} ${name}`);
+}
+
+myGreeting("Walker");*/
+
+// spread operator
+/*let numbers = [1,2,3,4,5];
+
+let minnum = Math.min(...numbers)
+console.log(minnum);*/
+
+// with strings 
+
+/*let name = "Walker";
+let letters = [...name].join(".");
+console.log(letters);*/
+
+// can be used to combine two arrays
+
+/*let fruits = ["apple" , "orange"];
+let vegetables = ["carrot" , "beetroot"];
+
+let food =[...fruits , ...vegetables , "egg" , "milk"];
+console.log(food);*/
+
+// callbacks 
+
+/*function sum(x, y, callback){
+    let result = x + y;
+    callback(result);
+}
+
+function displayResult(sum){
+    document.getElementById("head-el").textContent = sum;
+}
+
+sum(5,10 , displayResult);*/
+
+
+//foreach(callback)
+
+/*let numbers = [1,2,3,4,5]
+
+numbers.forEach(double);
+numbers.forEach(display);
+function double (element, index,array){
+    (array[index] = element * 2);
+}
+
+function display(elements){
+    console.log(elements);
+}*/
+
+
+/*let fruits = [ "mango", "orange", "pawpaw"];
+
+fruits.forEach(display);
+
+function display(elements){
+    let bigger = elements.toUpperCase();
+    console.log(bigger);
+}*/
+
+let numbers = [1,2,3,4,5];
+
+const squares = numbers.map(square);
+const cubes = numbers.map(cube)
+console.log(squares);
+console.log(cubes)
+function square(elements){
+    return Math.pow(elements,2);
+}
+
+function cube(elements){
+    return Math.pow(elements,3); 
+}
--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-let user = {
+//Objects 
+
+let myObject = {};//Object literal syntax
+
+let myUser = new Object();//Object constructor
+
+
+/*let user = {
     name : "Walker",
     surname : "Johnson",
+    age :  30
 };
 
-console.log(user);
+console.log(user.age);
+
+// add another key-value pair to the existing object
+
+user.city = "New York";
+
+console.log(user.city);
+*/
+
+//How to get those flags
+
+//let descriptor = Object.getOwnPropertyDescriptor(obj, propertyName);
+
+//let descriptor = Object.getOwnPropertyDescriptor(user, "name");
+
+//console.log(descriptor);
+
+//using getters and setters
+// get and set;
+
+// let obj ={
+//     get propName(){},
+//     set propName(value){}
+// };
+
+let user = {
+    name : "Johnson",
+    surname : "Walker",
+
+    get fullname(){
+        return this.name + " " + this.surname;
+    },
+
+    set fullname(value){
+        [this.name , this.surname] = value.split(" ");
+    }
+};
+
+
+user.fullname = "Lucy Dumordzi";
+console.log(user.fullname);
