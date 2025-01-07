2df8377076c757aa9a661a663e210710098b1995
Johnson <sakajohnson1@gmail.com>
2025-01-07 19:26:39 +0000

    Javascript promises section1

--- Classes.js
+++ Classes.js
@@ -1,5 +1,5 @@
 //Exercise 1 Easy level
-class Person{
+/*class Person{
   static count = 0;
   constructor(name, age){
     this.name = name;
@@ -33,4 +33,53 @@ const person1 = new Person("Alice", 30);
 const person2 = new Person("Bob", 25);
 
 
-console.log(Person.PersonCount());
+console.log(Person.PersonCount()); */
+
+//Exercise 2 Medium level
+
+/*class shape{
+  
+  calculateArea(){
+    return 0;
+  }
+
+ /* static SumOfAreas(){
+    let shapes = ["Circle" , "Rectangle"];
+    let sum = 0;
+    for(let i = 0 ; i< shapes.length ; i++){
+      sum += shapes[i].calculateArea();
+      console.log(sum) ;
+    }
+  }*/
+
+  
+
+
+/*class Circle extends shape{
+  constructor(radius){
+    super();
+    this.radius = radius;
+  }
+  calculateArea(){
+  return Math.PI * this.radius * this.radius;
+  }
+}
+class Rectangle extends shape{
+  constructor(length, width){
+    super();
+    this.length = length;
+    this.width = width;
+  }
+  calculateArea(){
+    return this.length * this.width ;
+  }
+}
+
+const shape1 = new Rectangle(24,34);
+console.log(shape1.calculateArea());
+
+const shape2 = new Circle(12);
+
+console.log(shape2.calculateArea()); */
+
+console.log(process.env.PWD);
