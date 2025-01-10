5ac7d5f6fea4ba1d5b828486b2a4f8cb4e5f932d
Johnson <sakajohnson1@gmail.com>
2025-01-10 11:02:55 +0000

    Using async function to fetch data

---
+++ phoneNumber.js
@@ -0,0 +1,30 @@
+/*const myarr = [1,2,3,4,5,6 ,7,8,9,0];
+let firstThree = myarr.slice(0,3);
+let secondThree = myarr.slice(3,6);
+let lastfour = myarr.slice(6,10);
+
+console.log(firstThree.join(''));
+console.log(secondThree.join(''));
+console.log(lastfour.join(''));*/
+//First solution but not accurate
+/*function createPhoneNumber(numbers){
+  return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`;
+}
+
+let PhoneNumber = createPhoneNumber([1,2,3,4,5,6,7,8,9,0]);
+
+console.log(PhoneNumber);*/
+
+//Original solution
+
+function createPhoneNumber(numbers){
+  let format = '(xxx) xxx-xxxx';
+
+  for(let i=0; i<numbers.length; i++){
+    format = format.replace('x' , numbers[i]);
+  }
+  return format;
+}
+
+let PhoneNumber = createPhoneNumber([1,2,3,4,5,6,7,8,9,0]);
+console.log(PhoneNumber);
